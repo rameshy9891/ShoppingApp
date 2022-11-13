3580ff0dce845317b3735b486fad5172ce865e0b
rameshy9891 <113420314+rameshy9891@users.noreply.github.com>
2022-11-13 21:41:01 +0530

    Add files via upload

---
+++ hurt-floor-672/javascript.js
@@ -0,0 +1,6 @@
+function myfun(){
+ let newim=   document.getElementById("pm").scr="https://tmetric.com/media/epxhwygn/img-advantages-project-management.svg";
+    // console.log("hii")
+    document.querySelector("#sw")=newim;
+}
+
